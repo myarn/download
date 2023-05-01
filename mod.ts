ce37e80ee8b9e41219a5259df8186b45ecf9e56c
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2023-05-01 22:29:14 +0900

    Move HashTransfomrStream to myarn/hashed_potato

--- mod.ts
+++ mod.ts
@@ -1,3 +1,2 @@
 export * from './Download.ts';
-export * from './transformStreams/HashTransformStream.ts';
 export * from './transformStreams/DownloadProgressStream.ts';
--- test/Download.test.ts
+++ test/Download.test.ts
@@ -1,6 +1,5 @@
 import { assert, assertEquals } from 'https://deno.land/std@0.185.0/testing/asserts.ts';
 import { Download } from '../Download.ts';
-import { HashTransformStream } from '../mod.ts';
 
 const targetResource = 'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf';
 
@@ -30,19 +29,6 @@ Deno.test('Download file', async () => {
     .downloadTo(TEST_FILE_DIRECTORY);
 
   assertEquals((await Deno.stat(TEST_FILE_DIRECTORY)).isFile, true);
-
-  await Deno.remove(TEST_FILE_DIRECTORY);
-});
-
-Deno.test('Download file and hash', async () => {
-  const response = await fetch(targetResource);
-  const hashStream = new HashTransformStream('SHA-512');
-
-  await new Download(response.body!)
-    .pipeThrough(hashStream)
-    .downloadTo(TEST_FILE_DIRECTORY);
-
-  assertEquals(hashStream.digestWithHex(), TEST_FILE_HASH);
 
   await Deno.remove(TEST_FILE_DIRECTORY);
 });
