0cc1a2d02b4b78a20f179301e488bd9296e2c105
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2023-05-05 16:33:06 +0900

    Update can change filesize

--- transformStreams/projector/DownloadProgress.ts
+++ transformStreams/projector/DownloadProgress.ts
@@ -1,29 +1,34 @@
 import { Line, ProgressBar, LoadingIndicator, Projector, toTemplateText } from '../../deps/projector.ts';
-import { filesize } from '../../deps/filesize.ts';
 import { FilesizeText } from './FilesizeText.ts';
 
 export class DownloadProgress extends Line {
   progressBar: ProgressBar;
   loadingIngicator: LoadingIndicator;
   nowSize: FilesizeText;
+  filesize: FilesizeText;
 
   constructor (
     projector: Projector,
     protected filename: string,
     value: number,
-    protected max: number) {
+    filesize: number) {
     super(projector);
 
-    this.progressBar = new ProgressBar(value, max, 20);
+    this.progressBar = new ProgressBar(value, filesize, 20);
     this.loadingIngicator = new LoadingIndicator();
     this.nowSize = new FilesizeText(value);
+    this.filesize = new FilesizeText(filesize);
 
-    this.addText(toTemplateText`[${this.progressBar}] ${this.loadingIngicator} ${filename} (${this.nowSize}/${filesize(this.max)})`);
+    this.addText(toTemplateText`[${this.progressBar}] ${this.loadingIngicator} ${filename} (${this.nowSize}/${this.filesize})`);
   }
 
   updateSize (size: number) {
     this.progressBar.value = size;
     this.nowSize.value = size;
+  }
+
+  updateFilesize (filesize: number) {
+    this.filesize.value = filesize;
   }
 
   stop () {
