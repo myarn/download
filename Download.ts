41bd1a2e4aa5514ef4eb646a58fbb4dc84ca50aa
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2023-07-27 00:50:15 +0900

    🎨 Update Download.ts

--- Download.ts
+++ Download.ts
@@ -1,8 +1,8 @@
 export class Download {
-  from: ReadableStream<Uint8Array>;
-
-  constructor (from: ReadableStream<Uint8Array>) {
-    this.from = from;
+  constructor (
+    public from: ReadableStream<Uint8Array>,
+    public to: string | URL | WritableStream<Uint8Array>
+  ) {
   }
 
   pipeThrough (transform: {
@@ -12,25 +12,33 @@ export class Download {
     return new Download(
       this.from.pipeThrough(
         transform, options
-      )
-    )
+      ),
+      this.to
+    );
   }
 
-  to (to: WritableStream<Uint8Array>) {
-    return this.from.pipeTo(to);
-  }
-
-  async downloadTo (to: string | URL) {
-    const tempPath = await Deno.makeTempFile({
-      suffix: '.jar'
-    });
-
-    const tempTo = await Deno.open(tempPath, {
-      write: true,
-      create: true
-    });
-
-    await this.to(tempTo.writable);
-    await Deno.rename(tempPath, to);
+  /**
+   * Start dowlonad task
+   * 
+   * If the `to` parameter is `WritableStream<Uint8Array>`, write directly to it.
+   * If `string | URL` is passed, it will be downloaded to the tmp directory and then moved to `to`.
+   * @param to Donwload Location/WritableStream<UintArray>
+   */
+  async start (to = this.to): Promise<void> {
+    if (to instanceof WritableStream) {
+      await this.from.pipeTo(to);
+    } else {
+      const tempPath = await Deno.makeTempFile({
+        suffix: '.jar'
+      });
+  
+      const tempTo = await Deno.open(tempPath, {
+        write: true,
+        create: true
+      });
+  
+      await this.from.pipeTo(tempTo.writable);
+      await Deno.rename(tempPath, to); 
+    }
   }
 }
